0ce5b7b6fb8792b016371f9d3d2ac3262c672f65
THOMAS RAMET <ramet.web@gmail.com>
2023-07-24 14:48:24 +0200

    94b

--- HundredElem.java
+++ HundredElem.java
@@ -3,10 +3,11 @@ package exo;
 public class HundredElem {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            System.out.print(i + " ");
-            if (i % 10 == 0) {
+            if (i % 10 == 0 && i != 0) {
                 System.out.println();
             }
+            System.out.print(i + " ");
+
         }
     }
 }
