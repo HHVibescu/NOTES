2cdef12540cf06f07867b6dd037c99e29fd8c479
HHVibescu <112878555+HHVibescu@users.noreply.github.com>
2023-07-24 09:54:25 +0200

    Create Slide75.java

---
+++ Slide75.java
@@ -0,0 +1,18 @@
+package exo;
+
+import java.util.Scanner;
+
+public class LetterNumberSpecialChar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Saisir un caractère :");
+        char c = scanner.next().charAt(0);
+        if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {
+            System.out.println(String.format("%s est une lettre de l'alphabet", c));
+        } else if (c >= '0' && c <= '9') {
+            System.out.println(String.format("%s est un chiffre.", c));
+        } else {
+            System.out.println(String.format("%s est un caractère spécial.", c));
+        }
+    }
+}
