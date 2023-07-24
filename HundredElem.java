7588b126d1c172fec6a53d4d92aad3863d321ffd
THOMAS RAMET <ramet.web@gmail.com>
2023-07-24 14:45:06 +0200

    94

---
+++ HundredElem.java
@@ -0,0 +1,12 @@
+package exo;
+
+public class HundredElem {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.print(i + " ");
+            if (i % 10 == 0) {
+                System.out.println();
+            }
+        }
+    }
+}
---
+++ Tablo.java
@@ -0,0 +1,14 @@
+package exo;
+
+public class Tablo {
+    public static void main(String[] args) {
+        int[] ages = new int[6];
+        ages[0] = 10;
+        ages[1] = 20;
+        ages[2] = 30;
+        ages[3] = 40;
+        ages[4] = 50;
+        ages[5] = 60;
+        System.out.println(ages[0]);
+    }
+}
